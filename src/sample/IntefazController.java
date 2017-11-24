55e9b6facefa83d66abaacb4aed56a6011f079d5
Wendy <gmwendy14@gmail.com>
2017-11-24 12:30:48 -0600

    Hoy

--- src/sample/IntefazController.java
+++ src/sample/IntefazController.java
@@ -2,7 +2,28 @@ package sample;
 
 
 
+import Colas.Cola;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
 
-public class IntefazController {
 
-    }
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+
+
+import javafx.scene.layout.HBox;
+
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+
+public class IntefazController implements Initializable {
+    @FXML
+    TextField BrowsTxt, InsertTxt, ExtraerTxt; // Declaración de los text fiel
+    @FXML
+    HBox Box ; // declaración del HBox
+
+    @FXML
+    Button BuscarBtn, InsertarBtn, ExtraerBtn, VaciarBtn, ContarBtn; //Declaración de los botones
